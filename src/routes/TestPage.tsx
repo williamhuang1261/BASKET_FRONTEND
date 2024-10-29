38835a454c1dad61158923aca09b52cbd761fa12
williamhuang1261 <153405658+williamhuang1261@users.noreply.github.com>
2024-10-28 22:06:41 -0400

    Adding Google LogIn

--- src/routes/TestPage.tsx
+++ src/routes/TestPage.tsx
@@ -1,28 +1,12 @@
-import { useEffect } from "react";
+import GoogleLogIn from "../components/Test/googleLogIn";
 
 const TestPage = () => {
-  const handleCallbackResponse = (res: any) => {
-    console.log(res.credential);
-  };
-  useEffect(() => {
-    /*global google*/
-    google.accounts.id.initialize({
-      client_id:
-        "1042653613547-54t2b8gkd3ukh21q55mpv3uqckrch4su.apps.googleusercontent.com",
-      callback: handleCallbackResponse,
-    });
-
-    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
-      theme: "outline",
-      size: "large",
-    });
-  }, []);
   return (
     <div>
       TestPage
-      <div id='signInDiv'></div>
+      <GoogleLogIn />
     </div>
-  )
-}
+  );
+};
 
-export default TestPage
+export default TestPage;
