a56a89bbdc8083b2d2b85d8d497c8bfe5a7791da
William <william.huang.1261@gmail.com>
2024-12-28 21:25:16 -0500

    Setting queryClient default options: staleTime -> Infinity

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./app.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Fallback from "./components/Loading/Fallback.tsx";
-import UserProvider from "./state/providers/UserProvider.tsx";
 import StatusProvider from "./state/providers/StatusProvider.tsx";
-
-const queryClient = new QueryClient();
+import UserProvider from "./state/providers/UserProvider.tsx";
+import App from "./app.tsx";
+import queryClient from "./services/queryClient.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
