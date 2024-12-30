b26d2509be737165cdcb1066d325a30184ce5558
William <william.huang.1261@gmail.com>
2024-12-30 00:03:38 -0500

    Refactor useWindowSize hook - update window size thresholds and enhance responsive ad rendering in MainPageAds and MainHeaderDiv components

--- src/components/General/Ads/MainPageAds.tsx
+++ src/components/General/Ads/MainPageAds.tsx
@@ -1,16 +1,22 @@
+import useWindowSize from "../../../hooks/useWindowSize";
 import CarouselAdDiv from "./CarouselAdDiv";
 import VerticalAd from "./VerticalAd";
 
 // TODO: Load ads conditionally according to window size
 const MainPageAds = () => {
+  const winSize = useWindowSize();
   return (
     <section className="mx-3 flex items-center gap-4 py-4 md:container md:mx-auto md:px-20">
-      <div className="hidden sm:block basis-full overflow-hidden rounded shadow-lg border-dark_gray lg:basis-9/13">
-        <CarouselAdDiv />
-      </div>
-      <div className="sm:hidden h-full sm:basis-4/13 overflow-hidden rounded shadow border-dark_gray lg:block ">
-        <VerticalAd />
-      </div>
+      {winSize >= 0 && (
+        <div className="hidden basis-full overflow-hidden rounded border-dark_gray shadow-lg sm:block lg:basis-9/13">
+          <CarouselAdDiv />
+        </div>
+      )}
+      {(winSize < 0 || winSize >= 2) && (
+        <div className="h-full overflow-hidden rounded border-dark_gray shadow sm:hidden sm:basis-4/13 lg:block ">
+          <VerticalAd />
+        </div>
+      )}
     </section>
   );
 };
--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,16 +1,28 @@
 import { useEffect, useState } from "react";
 
+/**
+ * @returns xs: -1 | sm: 0 | md: 1 | lg: 2 | xl: 3 | 2xl: 4 | 3xl: 5
+ * @description Returns the current window width with a number
+ * @example
+ * width: {
+ *   xs: -1 (width < 640),
+ *   sm: 0 (640 <= width < 768),
+ *   md: 1 (768 <= width < 1024),
+ *   lg: 2 (1024 <= width < 1280),
+ *   xl: 3 (1280 <= width < 1536),
+ *   2xl: 4 (1536 <= width < 2048),
+ *   3xl: 5 (2048 <= width)
+ * }
+ */
 const useWindowSize = () => {
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
-    height: window.innerHeight,
   });
 
   useEffect(() => {
     const handleResize = () => {
       setWindowSize({
         width: window.innerWidth,
-        height: window.innerHeight,
       });
     };
 
@@ -24,7 +36,10 @@ const useWindowSize = () => {
 
   const { width } = windowSize;
 
-  if (width < 768) {
+  if (width < 640) {
+    return -1;
+  }
+  if (640 <= width && width < 768) {
     return 0; // sm
   } else if (width >= 768 && width < 1024) {
     return 1; // md
